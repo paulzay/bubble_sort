fc3e4cd6433dd0e098474f35b59d76cbf9647b9c
paulzay <paulotieno2@gmail.com>
2020-06-17 21:00:31 +0300

    implement yield in feAture branch

--- sort.rb
+++ sort.rb
@@ -10,16 +10,15 @@ def bubble_sort(arr)
   arr
 end
 
-def bubble_sort_by(arrays)
-  arr = arrays
-  lg = 0
-  (arr.length - 1).times do |i|
-    (arr.length - i).times do |j|
-      lg = yield
-      arr[j], arr[j + 1] = arr[j + 1], arr[j] if lg > 1
+def bubble_sort_by(arr)
+  len = arr.size - 1
+
+  (0..len).map do |number|
+    (0..len - number - 1).map do |num|
+      num_next = num + 1
+      arr[num], arr[num_next] = arr[num_next], arr[num] if yield arr[num] > arr[num_next]
     end
   end
-
   arr
 end
 
@@ -27,4 +26,4 @@ arr = %w[hi hello hey]
 
 p bubble_sort_by(arr) { |right, left| right <=> left }
 
-pbubble_sort([1, 2, 5, 3, 6, 5, 6, 6, 7, 4, 3])
+p bubble_sort([1, 2, 5, 3, 6, 5, 6, 6, 7, 4, 3])
