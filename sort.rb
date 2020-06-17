438f0f8caba08f7cf5e95406603087c00647a891
paulzay <paulotieno2@gmail.com>
2020-06-17 18:03:15 +0300

    fix lint errors

--- sort.rb
+++ sort.rb
@@ -2,31 +2,29 @@ def bubble_sort(arr)
   len = arr.size - 1
 
   (0..len).map do |number|
-    (0..len - number - 1 ).map do |num_next|
-      num_next = number + 1 
-      arr[num_next], arr[number] = arr[number], arr[num_next] if  arr[number] > arr[num_next]
+    (0..len - number - 1).map do |num|
+      num_next = num + 1
+      arr[num], arr[num_next] = arr[num_next], arr[num] if arr[num] > arr[num_next]
     end
   end
   arr
 end
 
-
-def bubble_sort_by(ar)
-    arr = ar
-    lg = 0
-    (arr.length - 1).times do |i|
-       (arr.length - i).times do |j|
-         lg = yield
-         arr[j], arr[j+1] = arr[j+1], arr[j] if lg > 1
-       end
+def bubble_sort_by(arrays)
+  arr = arrays
+  lg = 0
+  (arr.length - 1).times do |i|
+    (arr.length - i).times do |j|
+      lg = yield
+      arr[j], arr[j + 1] = arr[j + 1], arr[j] if lg > 1
     end
+  end
 
-    arr
+  arr
 end
 
-arr = ["hi", "hello", "hey"]
+arr = %w[hi hello hey]
 
-p bubble_sort_by(arr) {|right, left| right <=> left }
+p bubble_sort_by(arr) { |right, left| right <=> left }
 
-
-# puts (bubble_sort([1, 2, 5, 3, 6, 5, 6, 6, 7, 4, 3]))
+pbubble_sort([1, 2, 5, 3, 6, 5, 6, 6, 7, 4, 3])
