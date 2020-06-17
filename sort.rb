76791c3e7e94271b1d4d1645bdffaeaa5fef9f03
= <=>
2020-06-17 17:27:15 +0300

    Sort by method got implemented

--- sort.rb
+++ sort.rb
@@ -10,4 +10,23 @@ def bubble_sort(arr)
   arr
 end
 
-puts (bubble_sort([1, 2, 5, 3, 6, 5, 6, 6, 7, 4, 3]))
+
+def bubble_sort_by(ar)
+    arr = ar
+    lg = 0
+    (arr.length - 1).times do |i|
+       (arr.length - i).times do |j|
+         lg = yield
+         arr[j], arr[j+1] = arr[j+1], arr[j] if lg > 1
+       end
+    end
+
+    arr
+end
+
+arr = ["hi", "hello", "hey"]
+
+p bubble_sort_by(arr) {|right, left| right <=> left }
+
+
+# puts (bubble_sort([1, 2, 5, 3, 6, 5, 6, 6, 7, 4, 3]))
