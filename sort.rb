21ec285a17758e3bc6d0c9109414c1b748d18115
keddo <adu.kedir.abdurahman@gmail.com>
2020-06-18 09:15:50 +0300

    Remove creating extra varibales

--- sort.rb
+++ sort.rb
@@ -2,9 +2,8 @@ def bubble_sort(arr)
   len = arr.size - 1
 
   (0..len).map do |number|
-    (0..len - number - 1).map do |num|
-      num_next = num + 1
-      arr[num], arr[num_next] = arr[num_next], arr[num] if arr[num] > arr[num_next]
+    (0...len - number).map do |num|
+      arr[num], arr[num + 1] = arr[num + 1], arr[num] if arr[num] > arr[num +1]
     end
   end
   arr
@@ -19,11 +18,10 @@ def bubble_sort_by(arr)
       new_arr[num], new_arr[num + 1] = new_arr[num + 1], new_arr[num] if compare > 1
     end
   end
-  arr
+  new_arr
 end
 
 arr = %w[hi hello hey]
-# unsorted = (1..10).to_a.reverse! , This is different scenario
 
 bubble_sort_by(arr) { |left, right| left.length - right.length }
 bubble_sort([1, 2, 5, 3, 6, 5, 6, 6, 7, 4, 3])
