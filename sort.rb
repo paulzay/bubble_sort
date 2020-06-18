9175b7108cab65b91b363ac511533d562bdfa5a8
keddo <adu.kedir.abdurahman@gmail.com>
2020-06-18 10:43:25 +0300

    make code compact, includes all positive number

--- sort.rb
+++ sort.rb
@@ -1,7 +1,5 @@
 def bubble_sort(arr)
-  len = arr.size - 1
-
-  (0...len).map do |number|
+  (0...arr.length - 1).map do |number|
     (0...len - number).map do |num|
       arr[num], arr[num + 1] = arr[num + 1], arr[num] if arr[num] > arr[num +1]
     end
@@ -10,17 +8,17 @@ def bubble_sort(arr)
 end
 
 def bubble_sort_by(arr)
-  len = arr.size - 1
   new_arr = arr
-  (0...len).map do |number|
+  (0...new_arr.length - 1).map do |number|
     (0...len - number).map do |num|
-      new_arr[num], new_arr[num + 1] = new_arr[num + 1], new_arr[num] if yield(new_arr[num], new_arr[num + 1]) > 1
+      new_arr[num], new_arr[num + 1] = new_arr[num + 1], new_arr[num] if yield(new_arr[num], new_arr[num + 1]) > 0
     end
   end
   new_arr
 end
 
 arr = %w[hi hello hey]
-
+unsorted = (1..10).to_a.reverse!
+bubble_sort_by(unsorted) { |left, right| left - right }
 bubble_sort_by(arr) { |left, right| left.length - right.length }
 bubble_sort([1, 2, 5, 3, 6, 5, 6, 6, 7, 4, 3])
